e7e53de28f12c8704a61a9f9cb1024bd35d3d83b
fogelmark <108455196+fogelmark@users.noreply.github.com>
2023-08-23 12:44:52 +0200

    context yolo

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/styles/global/main.scss'
-import CountContextProvider from './context/countContext.js'
 import { Provider } from 'react-redux'
 import { store } from './store/index.js'
+import CountContextProvider from './context/countContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
