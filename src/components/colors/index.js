4b99098c78756c3e0f81ea2be02ed9827978e15c
fogelmark <108455196+fogelmark@users.noreply.github.com>
2024-01-21 23:35:16 +0100

    Refactor exports

---
+++ src/components/colors/index.js
@@ -0,0 +1,29 @@
+const colors = [
+  {
+    name: 'red',
+    gradient: ['#85182A', '#B21E35'],
+    textColor: '#fff'
+  },
+  {
+    name: 'blue',
+    gradient: ['#0077B6', '#00B4D8'],
+    textColor: '#fff'
+  },
+  {
+    name: 'black',
+    gradient: ['#343A40', '#6C757D'],
+    textColor: '#fff'
+  },
+  {
+    name: 'green',
+    gradient: ['#2D6A4F', '#52B788'],
+    textColor: '#fff'
+  },
+  {
+    name: 'white',
+    gradient: ['#FFF2B2', '#FFFAE5'],
+    textColor: '#000'
+  },
+]
+
+export default colors
